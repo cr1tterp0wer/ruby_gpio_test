bfadb17f0785bd15662fa38f03d571779fc93bf5
Christopher Apodaca <capodacac@gmail.com>
2020-06-02 09:52:47 -0400

    test_signal_out.rb

--- test_signal_out.rb
+++ test_signal_out.rb
@@ -1,37 +1,53 @@
 =begin
-AT+IPR=115200
+AT+IPR=115200 #BAUD RATE
 AT+BAND=868500000
 AT+ADDRESS=1
 AT+NETWORKID=1
 =end
+#BAUD bits/second
+# TX serial is on "/dev/ttyAMA0"
+#
 
 module GPIO
   require 'rpi_gpio'
-  PIN_NUM = 5
+  LED_PIN_NUM = 29
+  UART0_TXD_PIN_NUM = 8
+  REYAX_ARGS = {
+    IPR: 115200,
+    BAND: 868500000,
+    ADDRESS: 1,
+    NETWORKID: 1
+  }.freeze
 
-  class GPIO_TEST
+  class GPIO_REYAX
 
     def initialize(led_on = false)
       @gpio = RPi::GPIO
-      @gpio.set_numbering :bcm
+      @gpio.set_numbering :board
       @gpio.clean_up
-      @gpio.setup PIN_NUM, as: :output, initialize: :low
+      @gpio.setup LED_PIN_NUM, as: :output, initialize: :low
+      @gpio.setup UART0_TXD_PIN_NUM, as: :output
+    end
+    
+    def broadcast
+      @previous = Time.now()
+      while true do 
+#        blink LED_PIN_NUM
+        @now = Time.now()
+        change = @now - @previous
+        @previous = @now
+        next if change < 0.00010416
+        puts change
+      end
     end
 
-    def blink
-      while true do 
-        sleep 1
-        if @gpio.high?(PIN_NUM)
-          puts 'HIGH'
-        else
-          puts 'LOW'
-        end
-        @gpio.high?(PIN_NUM) ? @gpio.set_low(PIN_NUM) : @gpio.set_high(PIN_NUM)
-      end
+    def blink( pin )
+      return if pin.nil?
+      @gpio.high?(pin) ? @gpio.set_low(pin) : @gpio.set_high(pin)
     end
   end
 end
 
 
-gpio = GPIO::GPIO_TEST.new
-gpio.blink
+gpio = GPIO::GPIO_REYAX.new
+gpio.broadcast

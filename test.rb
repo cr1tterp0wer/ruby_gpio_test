14315a59a565daa651c910f5c87ffe4ea2d27302
Christopher Apodaca <capodacac@gmail.com>
2020-06-01 22:37:15 -0400

    init

---
+++ test.rb
@@ -0,0 +1,30 @@
+module GPIO
+  require 'rpi_gpio'
+  PIN_NUM = 5
+
+  class GPIO_TEST
+
+    def initialize(led_on = false)
+      @gpio = RPi::GPIO
+      @gpio.set_numbering :bcm
+      @gpio.clean_up
+      @gpio.setup PIN_NUM, as: :output, initialize: :low
+    end
+
+    def blink
+      while true do 
+        sleep 1
+        if @gpio.high?(PIN_NUM)
+          puts 'HIGH'
+        else
+          puts 'LOW'
+        end
+        @gpio.high?(PIN_NUM) ? @gpio.set_low(PIN_NUM) : @gpio.set_high(PIN_NUM)
+      end
+    end
+  end
+end
+
+
+gpio = GPIO::GPIO_TEST.new
+gpio.blink
